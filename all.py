4d73bbe7fc1576326431b709541d21ceede4ba50
stacy <2586168620@qq.com>
2025-10-28 16:28:50 +0800

    comment one line

--- all.py
+++ all.py
@@ -4,4 +4,4 @@ import pytest
 ##用于执行测试案例
 if __name__ == '__main__':
     pytest.main(["./testcases/", "-sv", "--alluredir", "./temps"])
-    os.system("allure generate ./temps -o ./report --clean")
+    # os.system("allure generate ./temps -o ./report --clean")
